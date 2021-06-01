83977e74a5e2b973ce05540b0d1d753c43830254
Tayo Carvalhal <lucarv@Tayos-MacBook-Pro.local>
2021-06-01 09:40:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Azure Industrial Iot - OPC UA Deadband Filter
+
+This module can be chained on Azure IoT Edge in a pipeline after the opc publisher to effectively filter messages that fall within bookends.  
+For more info opn deadband, see the foundation website [here]](https://reference.opcfoundation.org/v104/Core/DataTypes/DeadbandType/)  
+As of the last commit of this repo, the feature was still in the backlog for Azure IIoT team. Whenever this feature is implemented this becomes obsolete.  
+
+## How does it work  
+You can configure the module in one of the two available modes, **Absolute** or **Percent**. This is controlled by setting the environmental variable DBF_TYPE (percent or absolute).  
+If the mode is percent, You can specify the percentage on the environmental variable (DBF_WEIGHT).  
+The absolute mode needs a config file contained the bookends as below:
+```
+[
+  {
+    "ApplicationUri": "<App Uri 1",
+    "OpcNodes": [
+      {
+        "Id": "i=<an integer>",
+        "DBF": {"min": 116, "max": 118}
+      },
+      {
+        "Id": "s=<a string>",
+        "DBF": {"min": 4, "max": 6}
+      },
+    ]
+  },
+  {
+    "ApplicationUri": "<App Uri 2",
+    "OpcNodes": [
+      {
+        "Id": "i=<an integer>",
+        "DBF": {"min": 116, "max": 118}
+      }
+    ]
+  },
+  <AS MANY ASSETS AS YOU NEED....>
+]
+```
+This file needs to exist on the iot edge host and must be mounted using container creation options as usual.  
+
+The default mode is PERCENT with a WEIGHT of 25%.  
+
+## How to use it
+Clone or download and create a module for your archtecture.  
+The log files can be used to give an accurate account of what messages have been filtered out. Set verbose by assigning the environmental variable DEBUG to true. You can toggle verbose also by adding a module desired property 'debug'.
---
+++ app.js
@@ -0,0 +1,152 @@
+'use strict';
+var debug = process.env.DEBUG || false;
+var dbfType = process.env.DBF_TYPE || 'percent';
+var dbfWeight = process.env.DBF_WEIGHT || 25;
+var filteredArray = [];
+
+if (dbfType == 'absolute') {
+  const jsonfile = require('jsonfile');
+  const file = '/appdata/dbf-config.json';
+  var tags = [];
+
+  jsonfile.readFile(file, function (err, obj) {
+    if (err) {
+      console.error(err);
+      console.log('Can\'t find config file nothis will be filtered');
+    } else {
+      tags = obj;
+    }
+  });
+}
+
+var cache = [];
+
+const getIndex = (els, elName, el) => {
+  var index = -1;
+  for (var i = 0; els.length > i; i += 1) {
+    if (els[i][elName] === el) {
+      index = i;
+      break;
+    }
+  }
+  return index;
+};
+
+const filterAbsolute = (client, payloadArray) => {
+  if (tags.length > 0) {
+    for (var i = 0; i < payloadArray.length; i++) {
+      let appURIindex = getIndex(tags, 'ApplicationUri', payloadArray[i].ApplicationUri);
+      if (appURIindex > -1) {
+        let nodeIDindex = getIndex(tags[appURIindex].OpcNodes, 'Id', payloadArray[i].NodeId);
+        if (nodeIDindex > -1) {
+          let min = tags[appURIindex].OpcNodes[nodeIDindex].DBF.min;
+          let max = tags[appURIindex].OpcNodes[nodeIDindex].DBF.max;
+          if ((payloadArray[i].Value.Value < min || payloadArray[i].Value.Value > max)) {
+            if (debug == true) console.log(`${payloadArray[i].ApplicationUri}:${payloadArray[i].NodeId} VALUE ${payloadArray[i].Value.Value} OUT OF BAND (${min}, ${max}), PUSH FORWARD`);
+            filteredArray.push(payloadArray[i]);
+          } else {
+            if (debug == true) console.log(`${payloadArray[i].ApplicationUri}:${payloadArray[i].NodeId}  VALUE ${payloadArray[i].Value.Value} INSIDE BAND (${min}, ${max}), IGNORE`);
+          }
+        } else {
+          if (debug == true) console.log(`${payloadArray[i].ApplicationUri}:${payloadArray[i].NodeId}  Not targetted for filtering`);
+          filteredArray.push(payloadArray[i]);
+        }
+      } else {
+        if (debug == true) console.log(`${payloadArray[i].ApplicationUri}:${payloadArray[i].NodeId}  Not targetted for filtering`);
+        filteredArray.push(payloadArray[i]);
+      }
+    }
+    var outputMsg = new Message(JSON.stringify(filteredArray));
+  } else {
+    var outputMsg = new Message(JSON.stringify(payloadArray));
+  }
+  client.sendOutputEvent('output1', outputMsg, () => { });
+
+}
+
+const filterPercent = (client, payloadArray) => {
+  let weight = dbfWeight / 100;
+
+  for (var i = 0; i < payloadArray.length; i++) {
+    let index = getIndex(cache, 'NodeId', payloadArray[i].NodeId);
+    if (index > -1) {
+      let min = (1 - weight) * cache[index].value;
+      let max = (1 + weight) * cache[index].value;
+      cache.splice(index, 1);
+      if ((payloadArray[i].Value.Value < min || payloadArray[i].Value.Value > max)) {
+        if (debug == true) console.log(`${payloadArray[i].ApplicationUri}:${payloadArray[i].NodeId} VALUE ${payloadArray[i].Value.Value} OUT OF BAND (${min}, ${max}), PUSH FORWARD`);
+        filteredArray.push(payloadArray[i]);
+      } else {
+        if (debug == true) console.log(`${payloadArray[i].ApplicationUri}:${payloadArray[i].NodeId} VALUE ${payloadArray[i].Value.Value} INSIDE BAND (${min}, ${max}), IGNORE`);
+      }
+    }
+    let cached = { 'NodeId': payloadArray[i].NodeId, 'value': payloadArray[i].Value.Value };
+    cache.push(cached);
+  }
+
+  if (debug == true) console.log(`${filteredArray.length} Values in Message after filtering`)
+
+  if (filteredArray.length > 0) {
+    var outputMsg = new Message(JSON.stringify(filteredArray));
+    client.sendOutputEvent('output1', outputMsg, () => { });
+  }
+}
+
+var Transport = require('azure-iot-device-mqtt').Mqtt;
+var Client = require('azure-iot-device').ModuleClient;
+var Message = require('azure-iot-device').Message;
+
+Client.fromEnvironment(Transport, function (err, client) {
+  if (err) {
+    throw err;
+  } else {
+    client.on('error', function (err) {
+      throw err;
+    });
+    // connect to the Edge instance
+    client.open(function (err) {
+      if (err) {
+        throw err;
+      } else {
+        console.log('IoT Hub module client initialized');
+        // Act on input messages to the module.
+        client.on('inputMessage', function (inputName, msg) {
+          pipeMessage(client, inputName, msg);
+        });
+
+        client.getTwin(function (err, twin) {
+          if (err) {
+            console.error('error getting twin: ' + err);
+          }
+          // Add a handler for desired property changes
+          twin.on('properties.desired', function (delta) {
+            if (debug == true) console.log('new desired properties received:' + JSON.stringify(delta));
+            if (delta.hasOwnProperty('debug')) {
+              debug = delta.debug;
+            }
+          });
+        });
+      }
+    });
+  }
+});
+
+// This function just pipes the messages without any change.
+function pipeMessage(client, inputName, msg) {
+  client.complete(msg, () => { });
+  var message = msg.getBytes().toString('utf8');
+  if (message) {
+
+    var payloadArray = JSON.parse(message);
+    filteredArray = [];
+    if (debug == true) console.log(`${payloadArray.length} Values in Message before filtering`)
+    if (debug == true) console.time('Filtering latency: ');
+    if (dbfType == 'percent') {
+      filterPercent(client, payloadArray);
+      if (debug == true) console.timeEnd('Filtering latency: ');
+    } else {
+      filterAbsolute(client, payloadArray);
+      if (debug == true) console.timeEnd('Filtering latency: ');
+    }
+  }
+}
